0c2942206e17e94d905998bc75a7c0c077cb5171
Alexander <kcedra@student.21-school.ru>
2021-06-03 01:38:54 +0300

    Added iterators. Need to finish other list functions

--- list.hpp
+++ list.hpp
@@ -6,56 +6,226 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 21:29:27 by                   #+#    #+#             */
-/*   Updated: 2021/05/31 23:00:23 by                  ###   ########.fr       */
+/*   Updated: 2021/06/03 01:35:24 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_CONTAINERS_LIST_H
 # define FT_CONTAINERS_LIST_H
 
-# include <memory>
-# include <cmath>
+# include <limits>
 
 namespace ft {
-	template<class T, class Alloc = std::allocator<T> >
+	template<class T>
 	class list {
 	private:
 		class node {
 		public:
-			T		*value;
+			T		value;
 			node	*next;
 			node	*prev;
-			Alloc	*falloc;
-			node(Alloc *alloc) : value(nullptr), next(nullptr), prev(nullptr), falloc(alloc){
-			};
-			node(Alloc *alloc, const T& val) : next(nullptr), prev(nullptr), falloc(alloc){
-				this->value = falloc->allocate(1);
-				falloc->construct(this->value, val);
-			};
-			node(Alloc *alloc, node &src) : next(nullptr), prev(nullptr), falloc(alloc){
-				this->value = falloc->allocate(1);
-				falloc->construct(this->value, *(src->value));
-			};
+
+			node() : value(0), next(nullptr), prev(nullptr) {};
+			node(const T& val) : value(val), next(nullptr), prev(nullptr) {};
+			node(node &src) : value(src.value), next(nullptr), prev(nullptr) {};
 		};
-		Alloc	_alloc;
 		node	*_Mnode;
 		size_t	_len;
+
 	public:
 		list() : _len(0){
-			this->_Mnode = new node(&(this->_alloc));
+			this->_Mnode = new node();
 		};
 		~list(){
+			this->clear();
+			delete this->_Mnode;
 		};
-		void 	pushback(const T& val){
-			node*	newNode = new node(&(this->_alloc), val);
 
-			newNode->next = _Mnode;
-			newNode->prev = _Mnode->prev;
-			_Mnode->prev = newNode;
-			if (_Mnode->next == nullptr)
-				_Mnode->next = newNode;
-			this->_len++;
+		/* Forward iterator class */
+		class iterator{
+		private:
+			T		*_value;
+			node	*_next;
+			node	*_prev;
+		public:
+			iterator() : _value (nullptr), _next(nullptr), _prev(nullptr){
+			};
+			iterator(const iterator &src) : _value(src._value), _next(src._next), _prev(src._prev){
+			};
+			~iterator(){
+			};
+
+			/* Operators */
+			iterator&	operator++(){
+				this->_value = &(this->_next->value);
+				this->_prev = this->_next->prev;
+				this->_next = this->_next->next;;
+				return (*this);
+			};
+			iterator&	operator++(int){
+				iterator	result(*this);
+
+				this->_value = this->_next->value;
+				this->_prev = this->_next->prev;
+				this->_next = this->_next->next;
+				return (result);
+			};
+			iterator&	operator--(){
+				this->_value = &(this->_prev->value);
+				this->_next = this->_prev->next;
+				this->_prev = this->_prev->prev;
+				return (*this);
+			};
+			iterator&	operator--(int){
+				iterator	result(*this);
+
+				this->_value = &(this->_prev->value);
+				this->_next = this->_prev->next;
+				this->_prev = this->_prev->prev;
+				return (result);
+			};
+			T&			operator*(){
+				return (*(this->_value));
+			};
+			T*			operator->() const {
+				return &(operator*());
+			}
+			bool 		operator!=(const iterator &rhs){
+				if (this->_next != rhs._next || this->_prev != this->_prev)
+					return (1);
+				else
+					return (0);
+			};
+			bool 		operator==(const iterator &rhs){
+				if (this->_next == rhs._next && this->_prev == this->_prev)
+					return (1);
+				else
+					return (0);
+			};
+			iterator&	operator=(const iterator &rhs){
+				this->_value = rhs._value;
+				this->_next = rhs._next;
+				this->_prev = rhs._prev;
+				return (*this);
+			};
+
+			friend iterator list<T>::begin();
+			friend iterator list<T>::end();
 		};
+
+		/* Reverse iterator class */
+		class reverse_iterator{
+		private:
+			T		*_value;
+			node	*_next;
+			node	*_prev;
+		public:
+			reverse_iterator() : _value (nullptr), _next(nullptr), _prev(nullptr){
+			};
+			reverse_iterator(const reverse_iterator &src) : _value(src._value), _next(src._next), _prev(src._prev){
+			};
+			~reverse_iterator(){
+			};
+
+			/* Operators */
+			reverse_iterator&	operator++(){
+				this->_value = &(this->_prev->value);
+				this->_next = this->_prev->next;
+				this->_prev = this->_prev->prev;
+				return (*this);
+			};
+			reverse_iterator&	operator++(int){
+				reverse_iterator	result(*this);
+
+				this->_value = &(this->_prev->value);
+				this->_next = this->_prev->next;
+				this->_prev = this->_prev->prev;
+				return (result);
+			};
+			reverse_iterator&	operator--(){
+				this->_value = &(this->_next->value);
+				this->_prev = this->_next->prev;
+				this->_next = this->_next->next;;
+				return (*this);
+			};
+			reverse_iterator&	operator--(int){
+				iterator	result(*this);
+
+				this->_value = this->_next->value;
+				this->_prev = this->_next->prev;
+				this->_next = this->_next->next;
+				return (result);
+			};
+			T&					operator*(){
+				return (*(this->_value));
+			};
+			T*					operator->() const {
+				return &(operator*());
+			}
+			bool				operator!=(const reverse_iterator &rhs){
+				if (this->_next != rhs._next || this->_prev != this->_prev)
+					return (1);
+				else
+					return (0);
+			};
+			bool 				operator==(const reverse_iterator &rhs){
+				if (this->_next == rhs._next && this->_prev == this->_prev)
+					return (1);
+				else
+					return (0);
+			};
+			reverse_iterator&	operator=(const reverse_iterator &rhs){
+				this->_value = rhs._value;
+				this->_next = rhs._next;
+				this->_prev = rhs._prev;
+				return (*this);
+			};
+
+			friend reverse_iterator list<T>::rbegin();
+			friend reverse_iterator list<T>::rend();
+		};
+
+
+		/* Iterators */
+		iterator			begin(){
+			iterator	ret;
+
+			if (_len != 0) {
+				ret._value = &(this->_Mnode->next->value);
+				ret._next = this->_Mnode->next->next;
+				ret._prev = this->_Mnode;
+			}
+			return (ret);
+		};
+		iterator			end(){
+			iterator	ret;
+
+			if (_len != 0) {
+				ret._next = this->_Mnode->next;
+				ret._prev = this->_Mnode->prev;
+			}
+			return (ret);
+		};
+		reverse_iterator	rbegin(){
+			reverse_iterator	ret;
+
+			if (_len != 0) {
+				ret._value = &(this->_Mnode->prev->value);
+				ret._prev = this->_Mnode->prev->prev;
+				ret._next = this->_Mnode;
+			}
+			return (ret);
+		};
+		reverse_iterator	rend(){
+			reverse_iterator	ret;
+
+			if (_len != 0) {
+				ret._next = this->_Mnode->next;
+				ret._prev = this->_Mnode->prev;
+			}
+			return (ret);
+		};
+
 		/* Capacity */
 		bool	empty() const{
 			if (this->_len == 0)
@@ -67,21 +237,76 @@ namespace ft {
 			return (this->_len);
 		};
 		size_t	max_size() const{
-			return ((size_t)pow(2, 64)/sizeof(T) - 1);
+			return (std::numeric_limits<T>::max());
 		};
-		/* Element access */
 
+		/* Element access */
 		T&		front(){
-			return (*(_Mnode->next->value));
+			return (this->_Mnode->next->value);
 		};
 		T&		back(){
-			return (*(_Mnode->prev->value));
+			return (this->_Mnode->prev->value);
 		};
-		/* Observers */
 
-		std::allocator<T>	get_allocator(){
-			return (this->_alloc);
+		/* Modifiers */
+		void 	pop_front(){
+			node*	address = this->_Mnode->next;
+
+			this->_Mnode->next = address->next;
+			address->next->prev = this->_Mnode;
+			delete address;
+			this->_len--;
+			if (this->_len == 0){
+				this->_Mnode->next = nullptr;
+				this->_Mnode->prev = nullptr;
+			}
 		};
+		void 	push_front(const T& val){
+			node*	newNode = new node(val);
+
+			newNode->prev = this->_Mnode;
+			if (this->_len == 0)
+				newNode->next = this->_Mnode;
+			else{
+				newNode->next = this->_Mnode->next;
+				newNode->next->prev = newNode;
+			}
+			this->_Mnode->next = newNode;
+			if (this->_Mnode->prev == nullptr)
+				this->_Mnode->prev = newNode;
+			this->_len++;
+		};
+		void 	pop_back(){
+			node*	address = this->_Mnode->prev;
+
+			this->_Mnode->prev = address->prev;
+			address->prev->next = this->_Mnode;
+			delete address;
+			this->_len--;
+			if (this->_len == 0){
+				this->_Mnode->next = nullptr;
+				this->_Mnode->prev = nullptr;
+			}
+		};
+		void 	push_back(const T& val){
+			node*	newNode = new node(val);
+
+			newNode->next = this->_Mnode;
+			if (this->_len == 0)
+				newNode->prev = this->_Mnode;
+			else{
+				newNode->prev = this->_Mnode->prev;
+				newNode->prev->next = newNode;
+			}
+			this->_Mnode->prev = newNode;
+			if (this->_Mnode->next == nullptr)
+				this->_Mnode->next = newNode;
+			this->_len++;
+		};
+		void 	clear(){
+			while (!this->empty())
+				this->pop_front();
+		}
 	};
 }
 #endif //FT_CONTAINERS_LIST_H
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By:  <kcedra>                                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 20:15:32 by kcedra            #+#    #+#             */
-/*   Updated: 2021/05/31 23:16:55 by                  ###   ########.fr       */
+/*   Updated: 2021/06/03 01:28:07 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,30 +26,76 @@ int main() {
 	//std::cout << "Normal list Allocator max size = : " << a1.get_allocator().max_size() << std::endl;
 	std::cout << "My list tests ----------------------------------------" << std::endl;
 	std::cout << "Container is empty? : " << test.empty() << std::endl;
-	test.pushback(3);
-	test.pushback(4);
-	std::cout << test.back() << std::endl;
-	test.pushback(2);
-	std::cout << test.back() << std::endl;
-	std::cout << "Changing back to 5" << std::endl;
-	test.back() = 5;
-	std::cout << test.back() << std::endl;
+	test.push_back(3);
+	test.push_back(4);
+	//std::cout << test.back() << std::endl;
+	test.push_back(2);
+	//std::cout << test.back() << std::endl;
+	//std::cout << "Changing back to 5" << std::endl;
+	//test.back() = 5;
+	//std::cout << test.back() << std::endl;
+	//std::cout << test.front() << std::endl;
+	test.push_front(15);
+	//std::cout << test.front() << std::endl;
+	std::cout << "Print container len: " << test.size() << std::endl;
+	//std::cout << "Container is empty? : " << test.empty() << std::endl;
+	//std::cout << "Popping front" << std::endl;
+	test.pop_front();
+	test.pop_front();
 	std::cout << test.front() << std::endl;
 	std::cout << "Print container len: " << test.size() << std::endl;
-	std::cout << "Container is empty? : " << test.empty() << std::endl;
+	std::cout << "Popping back" << std::endl;
+	test.pop_back();
+	std::cout << test.back() << std::endl;
+	test.push_back(10);
+	test.push_back(15);
+	test.push_back(20);
+	test.push_back(25);
+	test.push_back(30);
+	//ft::list<int>::iterator	it;
+	//ft::list<int>::iterator	ite;
+	std::cout << "Before testing iterators" << std::endl;
+	std::cout << "Print container len: " << test.size() << std::endl;
+	std::cout << "Forward iterators test --------------" << std::endl;
+	for (ft::list<int>::iterator it = test.begin(); it != test.end(); ++it)
+		std::cout << "Value = " << *it << std::endl;
+	std::cout << "Forward iterators test end ----------" << std::endl;
+	std::cout << "Reverse iterators test --------------" << std::endl;
+	for (ft::list<int>::reverse_iterator it = test.rbegin(); it != test.rend(); ++it)
+		std::cout << "Value = " << *it << std::endl;
+	std::cout << "Reverse iterators test end ----------" << std::endl;
+	std::cout << "Print container len: " << test.size() << std::endl;
+	std::cout << "After testing iterators" << std::endl;
+	std::cout << "Print container len: " << test.size() << std::endl;
+//	while (!test.empty()){
+//		std::cout << "Front: " << test.front() << std::endl;
+//		test.pop_front();
+//	}
+	std::cout << "Clear tests --------------------------" << std::endl;
+	test.push_back(30);
+	test.push_back(35);
+	test.push_back(40);
+	test.push_back(45);
+	std::cout << "Print container len: " << test.size() << std::endl;
+	std::cout << "Clear happens here" << std::endl;
+	test.clear();
+	std::cout << "Print container len: " << test.size() << std::endl;
+	std::cout << "Clear tests end ----------------------" << std::endl;
 	std::cout << "------------------------------------------------------" << std::endl;
-	a1.push_back(2);
-	a1.push_back(3);
-	test1 = a1.end();
-	end1 = --a1.begin();
-	test2 = a1.rend();
-	if (test1 == end1)
-		std::cout << "I'm here" << std::endl;
-	std::cout << "This is first end: " << *test1 << "; and this is second one: " << *test2 << std::endl;
+//	a1.push_back(2);
+//	a1.push_back(3);
+	test1 = a1.begin();
+//	std::cout << a1.size() << std::endl;
+//	std::cout << *test1 << std::endl;
+//	end1 = --a1.begin();
+//	test2 = a1.rend();
+//	if (test1 == end1)
+//		std::cout << "I'm here" << std::endl;
+//	std::cout << "This is first end: " << *test1 << "; and this is second one: " << *test2 << std::endl;
 	std::cout << "My max_size() : " << test.max_size() << "; and list std max_size() : " << a1.max_size() << std::endl;
-	std::cout << test.max_size() - a1.max_size() << std::endl;
-	std::cout << (int)sqrt(test.max_size()) << std::endl;
-	std::cout << (int)sqrt(a1.max_size()) << std::endl;
+//	std::cout << test.max_size() - a1.max_size() << std::endl;
+//	std::cout << (int)sqrt(test.max_size()) << std::endl;
+//	std::cout << (int)sqrt(a1.max_size()) << std::endl;
 	//std::cout << "List allocator max size = " << a1.get_allocator().max_size() << " and a1 max size = " << a1.max_size() << std::endl;
 	//std::cout << a1.get_allocator().max_size() - a1.max_size() << std::endl;
 	//std::cout << a1.get_allocator().max_size() / 2;
